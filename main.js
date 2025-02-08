b47d8eef97de25ab0e1d4931831fcb4de43708a2
paacs <103451242+paacs@users.noreply.github.com>
2025-02-08 15:48:52 +0100

    Button UI working

--- main.js
+++ main.js
@@ -26,49 +26,63 @@ function getHumanChoice() {
 
 
 // game logic
-function playGame() {
-    // declare and initialize score variavbles
-    let humanScore = computerScore = 0;
+// declare and initialize score variavbles
+let humanScore = computerScore = 0;
 
-    function playRound(humanChoice, computerChoice) {
-        // announce choices
-        console.log("You: " + humanChoice + "\nComputer: " + computerChoice);
-        // tie
-        if (humanChoice == computerChoice) {
-            console.log("It's a tie!");
-        }
-        // human wins
-        else if ((humanChoice == "rock" && computerChoice == "scissors")
-            || (humanChoice == "paper" && computerChoice == "rock")
-            || (humanChoice == "scissors" && computerChoice == "paper")) {
-            console.log("You win this round!");
-            humanScore++;
-        }
-        // computer wins
-        else {
-            console.log("You lose the round.");
-            computerScore++;
-        }
+let gameResult = document.querySelector("#gameResult");
+let playerScore = document.querySelector("#playerScore");
+let compScore = document.querySelector("#compScore");
+let choiceTxt = document.querySelector("#choiceTxt");
+let roundResult = document.querySelector("#roundResult");
+
+gameResult.textContent = "";
+
+
+function playRound(humanChoice, computerChoice) {
+
+    // reset if new game
+    if (humanScore == 5 || computerScore == 5) {
+        humanScore = computerScore = 0;
+        playerScore.textContent = compScore.textContent = 0;
+        gameResult.textContent = "";
+    }
+    // clear text from previous round
+    choiceTxt.textContent = "";
+    roundResult.textContent = "";
+    // tie
+    if (humanChoice == computerChoice) {
+        choiceTxt.textContent = humanChoice + " ties with " + computerChoice;
+        roundResult.textContent = "It's a tie...";
+    }
+    // human wins
+    else if ((humanChoice == "rock" && computerChoice == "scissors")
+        || (humanChoice == "paper" && computerChoice == "rock")
+        || (humanChoice == "scissors" && computerChoice == "paper")) {
+        choiceTxt.textContent = humanChoice + " beats " + computerChoice;
+        roundResult.textContent = "You win this round!";
+        humanScore++;
+        playerScore.textContent = humanScore;
+    }
+    // computer wins
+    else {
+        choiceTxt.textContent = computerChoice + " beats " + humanChoice;
+        roundResult.textContent = "You lose the round.";
+        computerScore++;
+        compScore.textContent = computerScore;
     }
 
-    console.log("Five rounds... GO!!!")
-
-    for (let i = 1; i < 6; i++) {
-        // get choices
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
-
-        console.log("*** Round " + i + " ***");
-        playRound(humanChoice, computerChoice);
-        
-        console.log("Your score: " + humanScore + "\nComputer's score: " + computerScore);
-    }
-
-    if (humanScore > computerScore) {
-        console.log("YOU ARE THE WIENER!");
-    } else {
-        console.log("Awww, you lose!");
+    if (humanScore == 5 || computerScore == 5) {
+        if (humanScore == 5) {
+            gameResult.textContent = "You won!!!";
+        } else {
+            gameResult.textContent = "You lose!";
+        }
     }
 }
 
-playGame();
+
+document.addEventListener("click", (e) => {
+    if (e.target.id == "rock" || e.target.id == "paper" || e.target.id == "scissors") {
+        playRound(e.target.id, getComputerChoice());
+    }
+})
